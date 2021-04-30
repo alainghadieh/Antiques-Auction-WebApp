8707d9abc59611d7a7e898a398f1141080d0c5eb
Alain Ghadieh <alainghadieh@icloud.com>
2021-04-30 21:04:06 +0300

    Initial commit

---
+++ Antiques-Auction-WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Antiques_Auction_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
