36af41824de942c5ddd3e6b8dc07927452a3b404
Alain Ghadieh <alain.ghadieh@icloud.com>
2021-05-02 02:41:24 +0300

    Return all antique items in table

---
+++ Antiques-Auction-WebApp/Controllers/DashboardController.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using Antiques_Auction_WebApp.Models;
+using Antiques_Auction_WebApp.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Antiques_Auction_WebApp.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class DashboardController : Controller
+    {
+        private readonly AntiqueItemService _antqSvc;
+
+        public DashboardController(AntiqueItemService antiqueItemService)
+        {
+            _antqSvc = antiqueItemService;
+        }
+
+        public ActionResult<IList<AntiqueItem>> Index() => View(_antqSvc.Read());
+    }
+}
+
