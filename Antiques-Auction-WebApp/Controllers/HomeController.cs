9dc0d6eba31eafbaeaa13cae27f89aad55e6c5fb
Alain Ghadieh <alain.ghadieh@icloud.com>
2021-05-02 02:32:19 +0300

    Return antique items for sale in table

--- Antiques-Auction-WebApp/Controllers/HomeController.cs
+++ Antiques-Auction-WebApp/Controllers/HomeController.cs
@@ -3,28 +3,28 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Antiques_Auction_WebApp.ViewModels;
 using Microsoft.AspNetCore.Authorization;
+using Antiques_Auction_WebApp.Services;
+using System.Collections.Generic;
+using Antiques_Auction_WebApp.Models;
 
 namespace Antiques_Auction_WebApp.Controllers
 {
-    [Authorize]
+    [Authorize(Roles = "Regular")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AntiqueItemService _antqSvc;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(AntiqueItemService antiqueItemService, ILogger<HomeController> logger)
         {
+            _antqSvc = antiqueItemService;
             _logger = logger;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        public IActionResult Index()  
+        {  
+            return View(_antqSvc.GetItemsForSale());  
+        }  
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
