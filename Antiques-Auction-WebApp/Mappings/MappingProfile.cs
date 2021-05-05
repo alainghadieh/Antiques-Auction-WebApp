677e13b04a90400f56b10c399acaceceaa0b8b3c
Alain Ghadieh <alain.ghadieh@icloud.com>
2021-05-05 15:01:32 +0300

    Add MappingProfile class and configure AutoBidConfig mapping

---
+++ Antiques-Auction-WebApp/Mappings/MappingProfile.cs
@@ -0,0 +1,14 @@
+using Antiques_Auction_WebApp.Models;
+using Antiques_Auction_WebApp.ViewModels;
+using AutoMapper;
+
+namespace Antiques_Auction_WebApp.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<AutoBidConfig, AutoBidConfigViewModel>().ReverseMap();
+        }
+    }
+}
