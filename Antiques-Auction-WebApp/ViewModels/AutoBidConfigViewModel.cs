fb5a3b74b17f2c6399e91254a846f142c5810e27
Alain Ghadieh <alain.ghadieh@icloud.com>
2021-05-06 05:57:06 +0300

    Fix layout and validation for auto-bidding configuration form

--- Antiques-Auction-WebApp/ViewModels/AutoBidConfigViewModel.cs
+++ Antiques-Auction-WebApp/ViewModels/AutoBidConfigViewModel.cs
@@ -6,9 +6,9 @@ namespace Antiques_Auction_WebApp.ViewModels
     public class AutoBidConfigViewModel
     {
         public string Id { get; set; }
-        [Display(Name = "Choose maximum amount allowed for auto-bidding"), Required]
+        [Display(Name = "Maximum amount for auto-bidding"), Required]
         public int MaxBidAmount { get; set; }
-        [Display(Name = "Choose alert threshold when a certain percentage of the max amount is reserved"), Required]
+        [Display(Name = "Percentage of the maximum bid amount reserved"), Required]
         [Range(typeof(int), "0", "100", ErrorMessage = "Threshold can only be between 0 and 100")]
         public int AlertThreshold { get; set; }
         public string UserName { get; set; }
